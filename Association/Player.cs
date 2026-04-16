ecd6a8d9ad23b3ed20a08ee61702b5e170b0ee61
oleksiy-dyshlevyy <oleksiy.dyshlevyy@gmail.com>
2026-04-16 19:12:11 +0300

    Add files via upload

---
+++ Association/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLec2Example.Association
+{
+    public class Player
+    {
+        public Player()
+        { }
+
+    }
+}
---
+++ Association/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLec2Example.Association
+{
+    public class Team
+    {
+        Player player;
+        public Team()
+        { }
+
+        void Transfer(Player player)
+        {
+          //  this.player = player;
+        }
+    }
+}
