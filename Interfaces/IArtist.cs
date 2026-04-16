a5642d6debdf4d7d9012b3818001ef9903cf057f
oleksiy-dyshlevyy <oleksiy.dyshlevyy@gmail.com>
2026-04-16 17:59:47 +0000

    updates

---
+++ Interfaces/IArtist.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace RelAbsInt.Interfaces;
+
+    public interface IArtist
+    {
+        void SingSong();
+        void Dance();
+        void DeclaimPoem();
+        int VoiceType
+        {
+            get;     
+            set;    
+        }
+
+    }
+
+    // public interface ISingable
+    // {
+    //     void SingSong(string songName);
+    // }
+
+    // public interface IDanceable
+    // {
+    //     void Dance();
+    // }
+
+    // public interface IPoetryDeclamation
+    // {
+    //     void DeclaimPoem();
+
+    // }
+
+    // public class Singer : ISingable, IDanceable
+    // {
+    //     public void SingSong(string songName)
+    //     {
+    //         Console.WriteLine(songName);
+    //     }
+    //     public void Dance()
+    //     {
+    //     }
+
+    // }
+
+    // public class Band: ISingable, IPoetryDeclamation
+    // {
+    //     public void SingSong(string songName)
+    //     {
+    //         Console.WriteLine(songName);
+    //     }
+    //     public void DeclaimPoem()
+    //     {
+
+    //     }
+    // }
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+ using RelAbsInt.Interfaces;
+
+
+class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+           
+            #region Lec - Interfaces
+
+            
+
+
+
+             UkrainianArtist person1 = new UkrainianArtist();
+        //     FranchArtist person2 = new FranchArtist();
+        //     person1.Magic();
+
+        //     IArtist p;// = new UkrainianArtist();
+            
+        //     p = person1;
+        //     p.SingSong();
+        //     p = person2;
+        //     p.SingSong();
+
+        //     AB ab = new AB();
+        //     ab.Method();
+        //     ab.Method1();
+          
+        //     InterfaceA interfaceA = new AB();
+        //     interfaceA.Method();
+        //    // interfaceA.Method1();
+
+        //     InterfaceB interfaceB = new AB();
+        //     interfaceB.Method();
+
+        //     char cc = ab.MethodA();
+        //     cc = ab.MethodB();
+
+            #endregion
+           
+           
+        }
+    }
